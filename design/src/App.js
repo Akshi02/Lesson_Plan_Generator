47e885ab99eb643e540af53f54351a0581dbcc10
Akshita Sushil Bhatia <asb272@uowmail.edu.au>
2024-07-01 20:04:24 +0400

    Modified App.js

--- design/src/App.js
+++ design/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LessonPlanForm from './LessonPlanForm';
+import LessonPlanResult from './LessonPlanResult';
 
 function App() {
+  const [lessonPlan, setLessonPlan] = React.useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lesson Plan Generator</h1>
+      <LessonPlanForm setLessonPlan={setLessonPlan} />
+      {lessonPlan && <LessonPlanResult lessonPlan={lessonPlan} />}
     </div>
   );
 }
