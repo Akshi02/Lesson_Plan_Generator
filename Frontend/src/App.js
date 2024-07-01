6ba9c51401fa3f5c08ac190618b8a7dc8e2e86a9
Akshita Sushil Bhatia <asb272@uowmail.edu.au>
2024-07-01 19:01:16 +0400

    Adding App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LessonPlanForm from './LessonPlanForm';
+import LessonPlanResult from './LessonPlanResult';
+
+function App() {
+  const [lessonPlan, setLessonPlan] = React.useState(null);
+
+  return (
+    <div className="App">
+      <h1>Lesson Plan Generator</h1>
+      <LessonPlanForm setLessonPlan={setLessonPlan} />
+      {lessonPlan && <LessonPlanResult lessonPlan={lessonPlan} />}
+    </div>
+  );
+}
+
+export default App;
